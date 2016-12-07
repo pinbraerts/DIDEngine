0a03922d1077d0183fc52f94dadfc97522409920
pinbraerts <shirshov.dima@gmail.com>
2016-12-07 20:00:06 +0300

    Revert "Revert "Тесты стали абстрагированнее. Теперь надо их писать.""
    This reverts commit d3b3810af2f94a4f933f114acb3715eb000282de.

--- DEBUGMain.cpp
+++ DEBUGMain.cpp
@@ -6,7 +6,9 @@
 #include "DIDEngineScriptingLanguage\DIDESLLexerTest.h"
 
 int main() {
-	DEBUG_LOG("test ", test() ? L"accepted" : L"rejected");
+	DEBUG_SEPARATOR = L"";
+	DEBUG_LOG("test #0 ", DIDESLLexerTestFile(0, L"TestScript0.dides", DIDESL::Lexer::Error::UNEXPECTED_EOF, DIDESL::Token::END) ? L"accepted" : L"rejected");
+	DEBUG_LOG("test #1 ", DIDESLLexerTest(1, DIDESL::Lexer::Error::EMPTY_LITERAL, DIDESL::Token::OBRACE, 2, 31) ? L"accepted" : L"rejected");
 	while (true);
 	return 0;
 }
