95c4eac6a206405f07dfc7ae7d74fc8aceed3ef1
pinbraerts <shirshov.dima@gmail.com>
2016-12-09 15:43:49 +0300

    Опять что-то лагануло. Перенёс макросы в отдельный файл.

---
+++ UTILS.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define __ARGS_SELECTOR_8(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
+#define __MACRO_BRACES(ARGS) __ARGS_SELECTOR_8 ARGS
+#define __MACRO_SELECTOR(TARGET, ...) __MACRO_BRACES((__VA_ARGS__, TARGET##_8, TARGET##_7, TARGET##_6, TARGET##_5, TARGET##_4, TARGET##_3, TARGET##_2, TARGET##_1, ))
+#define __CHOOSE_FROM_ARG(TARGET, ...) __MACRO_SELECTOR(TARGET, TARGET##_0 __VA_ARGS__ ())
+
+#endif // !UTILS_H
